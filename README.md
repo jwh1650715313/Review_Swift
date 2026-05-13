8ebe7b26a7854718cb0395536b659c059db1181e
jingwenhao <jingwenhao@kehwin.com>
2026-05-13 14:41:40 +0800

    初始化 Swift 项目
    添加 Xcode 工程、测试目标、中文 README 和 Git 忽略规则。

---
+++ README.md
@@ -0,0 +1,5 @@
+# Review_Swift
+
+Review_Swift 是一个基于 Swift 和 UIKit 创建的 iOS 示例项目。
+
+项目包含主应用、单元测试和 UI 测试目标，可作为后续学习、练习或功能开发的基础工程。
---
+++ Review_Swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Review_Swift
+//
+//  Created by kwmin on 2026/5/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
